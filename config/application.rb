2581a899bdf987d5980a173dfa2f8f96ff5f6aa0
Jonathan Meiss <jonathan@meiss.fr>
2013-03-10 18:08:45 +0100

    running app with facebook connect

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,14 @@ module CookBook
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Paris'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = :fr
+    config.i18n.default_locale = :fr
+
+    # If you are deploying Rails 3.1 on Heroku, you may want to set:
+    config.assets.initialize_on_precompile = false
   end
 end

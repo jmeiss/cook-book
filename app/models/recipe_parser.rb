c4c8671af142035f285b7d3819eb33399ad908a2
Jonathan Meiss <jonathan@meiss.fr>
2013-03-13 20:33:56 +0100

    can parse ingredients form marmiton json

--- app/models/recipe_parser.rb
+++ app/models/recipe_parser.rb
@@ -8,7 +8,7 @@ class RecipeParser
     URI.parse(url).host.gsub(/\./, '_')
   end
 
-  def self.get_json_from_www_marmition_org_url_id url_id 
+  def self.get_json_from_www_marmiton_org_url_id url_id 
     url   = "http://m.marmiton.org/webservices/json.svc/GetRecipeById?SiteId=1&RecipeId=#{url_id}"
     data  = Net::HTTP.get_response(URI.parse(url)).body
     JSON.parse data
@@ -16,14 +16,24 @@ class RecipeParser
 
   def self.www_marmiton_org url
     url_id    = url.scan(/\d+/).last
-    json_obj  = RecipeParser.get_json_from_www_marmition_org_url_id url_id
+    json_obj  = RecipeParser.get_json_from_www_marmiton_org_url_id url_id
     recipe    = json_obj['data']['items'][0]
 
-    Recipe.new  name: recipe['title'],
-                preparation_time: recipe['preparationTime'],
-                roasting_time: recipe['cookingTime'],
-                quantity: recipe['servings'],
-                url: url
+    recipe = Recipe.new name: recipe['title'],
+                        preparation_time: recipe['preparationTime'],
+                        roasting_time: recipe['cookingTime'],
+                        quantity: recipe['servings'],
+                        url: url
+    recipe
+  end
+
+  def self.get_www_marmiton_org_ingredients string
+    string.gsub('- ', '').split("\r\n")
+  end
+
+  def self.get_www_marmiton_org_directions string
+    return string.split('<br><br>') if string.include?('<br><br>')
+    string.split(" \r\n\r\n") if string.include?(" \r\n\r\n")
   end
 
 end
--- spec/models/recipe_parser_spec.rb
+++ spec/models/recipe_parser_spec.rb
@@ -10,11 +10,11 @@ describe RecipeParser do
     end
   end
 
-  describe 'www_marmiton_org' do
+  describe 'www.marmiton.org' do
     it "should return a Recipe object filled with marmiton recipe" do
       url       = 'http://www.marmiton.org/recettes/recette_les-timbales-de-jeanne-saumon-a-la-mousse-de-courgettes-au-micro-ondes_21864.aspx'
-      fake_json = Rails.root.join('spec', 'fixtures', 'm.marmiton.org-webservices-json.svc-GetRecipeById-SiteId-1-RecipeId-21864.json')
-      RecipeParser.any_instance.stub(:get_json_from_www_marmition_org_url_id).with('21864').and_return(File.read(fake_json))
+      fake_file = Rails.root.join('spec', 'fixtures', 'm.marmiton.org-webservices-json.svc-GetRecipeById-SiteId-1-RecipeId-21864.json')
+      RecipeParser.any_instance.stub(:get_json_from_www_marmiton_org_url_id).with('21864').and_return(File.read(fake_file))
       
       recipe  = Recipe.new  name: 'Les Timbales de Jeanne (saumon à la mousse de courgettes au micro-ondes)',
                             preparation_time: 10,
@@ -23,6 +23,32 @@ describe RecipeParser do
                             url: 'http://www.marmiton.org/recettes/recette_les-timbales-de-jeanne-saumon-a-la-mousse-de-courgettes-au-micro-ondes_21864.aspx'
 
       RecipeParser.www_marmiton_org(url).should eq(recipe)
+    end
+
+    it "should return an array of ingredients" do
+      url       = 'http://www.marmiton.org/recettes/recette_les-timbales-de-jeanne-saumon-a-la-mousse-de-courgettes-au-micro-ondes_21864.aspx'
+      fake_file = Rails.root.join('spec', 'fixtures', 'm.marmiton.org-webservices-json.svc-GetRecipeById-SiteId-1-RecipeId-21864.json')
+      RecipeParser.any_instance.stub(:get_json_from_www_marmiton_org_url_id).with('21864').and_return(File.read(fake_file))
+      fake_json = RecipeParser.get_json_from_www_marmiton_org_url_id('21864')
+
+      ingredients = ["4 tranches de saumon fumé", "2 courgettes", "3 oeufs", "10 cl de crème fraîche épaisse", 
+                      "aneth", "menthe", "1 gousse d'ail (petite)", "poivre, sel", "huile d'olive"]
+
+      RecipeParser.get_www_marmiton_org_ingredients(fake_json['data']['items'][0]['ingredientList']).should eq(ingredients)
+    end
+
+    it "should return an array of directions" do
+      url       = 'http://www.marmiton.org/recettes/recette_les-timbales-de-jeanne-saumon-a-la-mousse-de-courgettes-au-micro-ondes_21864.aspx'
+      fake_file = Rails.root.join('spec', 'fixtures', 'm.marmiton.org-webservices-json.svc-GetRecipeById-SiteId-1-RecipeId-21864.json')
+      RecipeParser.any_instance.stub(:get_json_from_www_marmiton_org_url_id).with('21864').and_return(File.read(fake_file))
+      fake_json = RecipeParser.get_json_from_www_marmiton_org_url_id('21864')
+      
+      directions = ["Râper les courgettes.", "Les faire revenir dans de l'huile d'olive avec l'ail et les herbes. Poivrer, et saler (mais pas trop, attention au saumon fumé !). Réserver, et laisser un peu refroidir.", 
+                    "Battre les oeufs et la crème en omelette.", "Mélanger l'omelette avec les courgettes, éventuellement, donner un petit coup de mixeur pour le côté \"mousse\".", 
+                    "Tapisser 4 ramequins avec les tranches de saumon fumé.", "Verser le mélange omelette-courgettes dans les ramequins tapissés de saumon.", 
+                    "Faire cuire au micro-ondes pendant 2 à 3 min, selon la puissance (vérifier la cuisson vous même, il n'y a que ça de vrai !)"]
+
+      RecipeParser.get_www_marmiton_org_directions(fake_json['data']['items'][0]['preparationList']).should eq(directions)
     end
   end
 end
